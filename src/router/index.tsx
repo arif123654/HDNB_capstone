09199b6da2b3816da8ecc3f60e7828e8486d8080
MD. Ariful Islam Anik <kmarif1432@gmail.com>
2025-01-22 18:06:32 +0600

    "Updating the routes"

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,10 @@
 import RootLayout from '@/pages/layout'
-import HomePage from '@/pages'
 import NotFound from '@/pages/not-found'
 import {createBrowserRouter} from 'react-router-dom'
+import ProductList from "@/pages/products/list/ProductList";
+import ProductDetail from "@/pages/products/details/ProductDetail";
+import Cart from "@/pages/cart/cart";
+import Login from "@/pages/login/login";
 
 export const router = createBrowserRouter([
   {
@@ -10,7 +13,23 @@ export const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <HomePage />
+        element: <ProductList />
+      },
+      {
+        path: '/products',
+        element: <ProductList />
+      },
+      {
+        path: '/products/:id',
+        element: <ProductDetail />
+      },
+      {
+        path: '/cart',
+        element: <Cart />
+      },
+      {
+        path: '/login',
+        element: <Login />
       }
     ]
   },
